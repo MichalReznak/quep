acfd489a6e81cb4c79d856e9710102d9f754eb42
Michal Režňák <reznak.michal@gmail.com>
2022-05-29 14:08:32 +0200

    Add ibmq

--- python/ibmq.py
+++ python/ibmq.py
@@ -8,6 +8,7 @@ class Ibmq:
     def __init__(self):
         self.backend = None
         self.circuit = None
+        self.circuits = []
 
     def auth(self):
         # IBMQ.enable_account()
@@ -19,6 +20,17 @@ class Ibmq:
         if log:
             print(self.circuit)
 
+    def append_circuit(self: 'Ibmq', circuit: str, log: bool):
+        self.circuits.append(QuantumCircuit.from_qasm_str(circuit))
+
+        if log:
+            for p in self.circuits:
+                print(p)
+
     def run(self: 'Ibmq') -> str:
         job = self.backend.run(self.circuit, shots=1024, memory=True)
         return job.result().get_counts(0)
+
+    def run_all(self: 'Ibmq') -> str:
+        job = self.backend.run(self.circuits, shots=1024, memory=True)
+        return job.result().get_counts()
--- src/qc_providers/ibmq.rs
+++ src/qc_providers/ibmq.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 use log::debug;
 use pyo3::prelude::*;
-use pyo3::types::PyDict;
+use pyo3::types::{PyDict, PyList};
 use pyo3::Python;
 use snafu::OptionExt;
 use tokio::time::{Duration, Instant};
@@ -33,10 +33,6 @@ impl IbmqQcProvider {
 #[async_trait]
 impl QcProvider for IbmqQcProvider {
     async fn connect(&mut self) -> Result<(), Error> {
-        Ok(())
-    }
-
-    async fn set_circuit(&mut self, circuit: String) -> Result<(), Error> {
         Python::with_gil(|py| -> Result<_, Error> {
             let code = std::fs::read_to_string(&format!("{}/ibmq.py", self.args.python_dir))?;
             let module = PyModule::from_code(py, &code, "", "")?;
@@ -44,9 +40,29 @@ impl QcProvider for IbmqQcProvider {
             let qiskit = qiskit.call0(py)?;
 
             qiskit.call_method0(py, "auth")?;
-            qiskit.call_method1(py, "set_circuit", (circuit, debug()))?;
-
             self.py_instance = Some(qiskit);
+            Ok(())
+        })
+    }
+
+    async fn set_circuit(&mut self, circuit: String) -> Result<(), Error> {
+        Python::with_gil(|py| -> Result<_, Error> {
+            self.py_instance.as_ref().context(OutOfBounds)?.call_method1(
+                py,
+                "set_circuit",
+                (circuit, debug()),
+            )?;
+            Ok(())
+        })
+    }
+
+    async fn append_circuit(&mut self, circuit: String) -> Result<(), Error> {
+        Python::with_gil(|py| -> Result<_, Error> {
+            self.py_instance.as_ref().context(OutOfBounds)?.call_method1(
+                py,
+                "append_circuit",
+                (circuit, debug()),
+            )?;
             Ok(())
         })
     }
@@ -70,12 +86,29 @@ impl QcProvider for IbmqQcProvider {
         })
     }
 
-    async fn append_circuit(&mut self, circuit: String) -> Result<(), Error> {
-        todo!()
-    }
-
     async fn run_all(&self) -> Result<Vec<String>, Error> {
-        todo!()
+        Python::with_gil(|py| {
+            let fun =
+                self.py_instance.as_ref().context(OutOfBounds)?.call_method0(py, "run_all")?;
+            let res = fun.cast_as::<PyList>(py).map_err(|_| PyDowncastError)?;
+            let res: Vec<_> = res
+                .into_iter()
+                .map(|e| e.cast_as::<PyDict>().map_err(|_| PyDowncastError).unwrap())
+                .map(|e| {
+                    let mut highest = ("".to_string(), 0);
+                    for (key, val) in e.into_iter() {
+                        println!("{key:#?}, {val:#?}");
+                        let val: i32 = val.extract().unwrap();
+
+                        if val > highest.1 {
+                            highest = (key.to_string(), val);
+                        }
+                    }
+                    format!("{}: {}", highest.0, highest.1)
+                })
+                .collect();
+            Ok(res)
+        })
     }
 
     fn start_measure(&mut self) {
